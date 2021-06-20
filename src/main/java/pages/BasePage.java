62bfab2f4f41019845be7095b68da9e4ec2e0f65
Kuzma <ivankuzmanovic87@gmail.com>
2021-06-20 09:37:18 +0200

    Setup BasePage and BaseTest with PropertyManager class and configuration.properties file for connecting chrome driver

---
+++ src/main/java/pages/BasePage.java
@@ -0,0 +1,46 @@
+package pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, 20);
+    }
+
+    // Wait for elements to be visible on the page
+    public void waitVisibility(By elementBy) {
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(elementBy));
+    }
+
+    // Click on the element
+    public void click(By elementBy) {
+        waitVisibility(elementBy);
+        driver.findElement(elementBy).click();
+    }
+
+    // Write text on the element
+    public void writeText(By elementBy, String text) {
+        waitVisibility(elementBy);
+        driver.findElement(elementBy).sendKeys(text);
+    }
+
+    // Read text from the element
+    public String readText(By elementBy) {
+        waitVisibility(elementBy);
+        return driver.findElement(elementBy).getText();
+    }
+
+    // Check if the two strings are equal
+    public void assertStringEquals(String string, String expectedText) {
+        Assert.assertEquals(string, expectedText);
+    }
+}
